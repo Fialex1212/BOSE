8c0763e137ec334db24a9c101a3ebb80f2bb9aa4
Alex <110390382+Fialex1212@users.noreply.github.com>
2023-11-10 22:07:52 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.querySelector(".menu").style.display = "block";
+    document.querySelector(".menu").style.position = "fixed";
+}
+
+function closeMenu() {
+    document.querySelector(".menu").style.display = "none";
+}
